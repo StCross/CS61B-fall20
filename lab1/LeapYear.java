6b41e0be56af48940b3592f8a1db53513a5c3adf
archer_jjm <archer_jjm@163.com>
2021-09-08 16:47:22 +0800

    modify leap year

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,13 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+      /** judge a year is leap year or not.
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) return true;
+        else return false;      
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
